3623a2c92f3c6ab3fd63e59ca2e24f068931e671
pleasehearm30ut <pleasehearm30ut@gmail.com>
2026-03-28 10:18:38 -0700

    Add GloryPhysics.js with gravitational mapping data

---
+++ src/axiom/GloryPhysics.js
@@ -0,0 +1,26 @@
+{
+  "Engine_ID": "AMEN_CHAIN_KINETIC_V1.2",
+  "Gravitational_Mapping": {
+    "Red_Letters": {
+      "Mass": 100,
+      "Behavior": "Sink to bottom (Gyroscope-dependent)"
+    },
+    "Glory_Words": {
+      "Mass": -100,
+      "Behavior": "Float to top edge (Anti-gravity)"
+    },
+    "Standard_Words": {
+      "Mass": 1,
+      "Behavior": "Slide and collide (X/Y Axis)"
+    }
+  },
+  "Collision_Pulse": {
+    "Trigger": "Screen_Wall_Impact",
+    "Haptic": "Tap",
+    "Visual": "Heavenly Silver Flash (#E5E4E2)"
+  },
+  "The_Reset": {
+    "Trigger": "Hara-Pivot (Shake/Snap)",
+    "Action": "Return Ions to Center / Clear Screen"
+  }
+}
